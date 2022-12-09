94894c6e7b5d9c0ba33f2e99e366836da3f296ba
Luca Bandiera <luca.bandiera3@studio.unibo.it>
2022-12-09 17:19:46 +0100

    Finished exercise 2

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
@@ -18,5 +20,57 @@ namespace ComplexAlgebra
     public class Complex
     {
         // TODO: fill this class\
+        public int Real { get; set; }
+        public int Imaginary { get; set; }
+
+        public Complex(int real, int imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }   
+
+        public double Modulus => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        
+        public double Phase => Math.Atan2(Imaginary, Real);
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+
+        protected bool Equals(Complex other)
+        {
+            return Real == other.Real && Imaginary == other.Imaginary;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Complex) obj);
+        }
+
+
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+
+        public Complex Plus(Complex other) => new Complex(Real + other.Real, Imaginary + other.Imaginary);
+
+        public Complex Minus(Complex other) => new Complex(Real - other.Real, Imaginary - other.Imaginary);
+
+        public override string ToString()
+        {
+            if (Imaginary == 0.0) return Real.ToString();
+            var ima = Math.Abs(Imaginary);
+            var val = ima == 1.0 ? "" : ima.ToString();
+            string s;
+            if (Real == 0.0)
+            {
+                s = Imaginary > 0 ? "" : "-";
+                return s + val + "i";
+            }
+
+            s = Imaginary > 0 ? " + " : " - ";
+            return $"{Real}{s}{val}i";
+        }
+
+        
     }
 }
